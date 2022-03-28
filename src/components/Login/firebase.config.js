248e08afc75b6b82738e82875abfd7d58939525a
Nazmus Sakib <nazmussakib161@gmail.com>
2022-03-28 22:21:43 +0600

    client site done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,22 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBHhOX8rtp2vahVcATkhTD_7CwARw6arrk",
+//     authDomain: "fire-auth2.firebaseapp.com",
+//     databaseURL: "https://fire-auth2.firebaseio.com",
+//     projectId: "fire-auth2",
+//     storageBucket: "fire-auth2.appspot.com",
+//     messagingSenderId: "587131087182",
+//     appId: "1:587131087182:web:f7e53e9007fe7760d7561f"
+//   };
+//   export default firebaseConfig;
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA0pDkXfyykx3R1qfxAVnoOxO2Ls3gGkzc",
+    authDomain: "ema-john-sampl.firebaseapp.com",
+    projectId: "ema-john-sampl",
+    storageBucket: "ema-john-sampl.appspot.com",
+    messagingSenderId: "158151128594",
+    appId: "1:158151128594:web:f8f8f9e96be93d90024acb"
+};
+
+export default firebaseConfig;
